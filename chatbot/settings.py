fa2aacac7d508930e3138ac29525d2f4f28c1135
24chance <chanceown@gmaile.com>
2026-02-13 20:34:59 +0200

    reeeeqs

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# For dev
+STATIC_URL = '/static/'
+
+# For production (collectstatic)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
